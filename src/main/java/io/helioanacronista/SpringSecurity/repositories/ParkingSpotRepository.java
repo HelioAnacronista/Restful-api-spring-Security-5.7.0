0a7fde6cc8b86fc2caff817a9f2ae489d1f5c39c
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-09-26 17:54:55 -0300

    start project

---
+++ src/main/java/io/helioanacronista/SpringSecurity/repositories/ParkingSpotRepository.java
@@ -0,0 +1,15 @@
+package io.helioanacronista.SpringSecurity.repositories;
+
+import io.helioanacronista.SpringSecurity.models.ParkingSpotModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+    boolean existsByLicensePlateCar(String licensePlateCar);
+    boolean existsByParkingSpotNumber(String parkingSpotNumber);
+    boolean existsByApartmentAndBlock(String apartment, String block);
+}
